4db00963a94dce08899663ea3d9afe14204225aa
christophe.duhamel.0909@gmail.com <christophe.duhamel.0909@gmail.com@58624a76-cb29-f74a-47b4-a1e6500350d0>
2014-11-19 10:08:07 +0000

    simplified database definition, added couple of packages to resources

--- trunk/Utils/database.py
+++ trunk/Utils/database.py
@@ -9,10 +9,7 @@ class Database:
     DATABASE_TABLE_NEPHROLOGISTS = 'nephrologists'
     DATABASE_TABLE_NEPHROLOGISTS_HOLIDAYS = 'nephrologists_holidays'
     DATABASE_TABLE_NEPHROLOGISTS_PREFERENCES = 'nephrologists_preferences'
-    DATABASE_TABLE_NEPHROLOGISTS_OBLIGATIONS = 'nephrologists_obligations'
     DATABASE_TABLE_MONTHLY_PLANNINGS = 'monthly_plannings'
-    DATABASE_TABLE_MONTHLY_PLANNINGS_WEEKS_DAYS = 'monthly_plannings_weeks_days'
-    DATABASE_TABLE_MONTHLY_PLANNINGS_NEPHROLOGISTS = 'monthly_plannings_nephrologists'
 
     @classmethod
     def __load__(cls):
@@ -36,6 +33,8 @@ class Database:
             connection.execute('''CREATE TABLE IF NOT EXISTS {} (
                 id_nephrologist_fk INTEGER NOT NULL,
                 timespan BLOB NOT NULL,
+                time_slot_type INTEGER NULL,
+                activity_type INTEGER NULL,
                 PRIMARY KEY(id_nephrologist_fk, timespan),
                 FOREIGN KEY(id_nephrologist_fk) REFERENCES {}(id)
             )'''.format(
@@ -44,70 +43,13 @@ class Database:
             ))
 
             connection.execute('''CREATE TABLE IF NOT EXISTS {} (
-                id_nephrologist_fk INTEGER NOT NULL,
-                timespan BLOB NOT NULL,
-                PRIMARY KEY(id_nephrologist_fk, timespan),
-                FOREIGN KEY(id_nephrologist_fk) REFERENCES {}(id)
-            )'''.format(
-                Database.DATABASE_TABLE_NEPHROLOGISTS_OBLIGATIONS,
-                Database.DATABASE_TABLE_NEPHROLOGISTS
-            ))
-
-            COLUMN_YEAR_PK = 'year_pk'
-            COLUMN_MONTH_PK = 'month_pk'
-            COLUMN_VERSION_PK = 'version_pk'
-            # TODO: don't forget to enforce unicity on rows {year, month} for column isReleasedVersion
-            connection.execute('''CREATE TABLE IF NOT EXISTS {0} (
-                {1} INTEGER NOT NULL,
-                {2} TEXT NOT NULL,
-                {3} INTEGER NOT NULL,
-                isReleasedVersion INTEGER NULL,
-                PRIMARY KEY({1}, {2}, {3})
-            )'''.format(
-                Database.DATABASE_TABLE_MONTHLY_PLANNINGS,
-                COLUMN_YEAR_PK,
-                COLUMN_MONTH_PK,
-                COLUMN_VERSION_PK
-            ))
-
-            connection.execute('''CREATE TABLE IF NOT EXISTS {0} (
-                {2} INTEGER NOT NULL,
-                {3} INTEGER NOT NULL,
-                {4} INTEGER NOT NULL,
-                {5} INTEGER NOT NULL,
-                {6} INTEGER NOT NULL,
-                {7} INTEGER NOT NULL,
-                {8} INTEGER NOT NULL,
-                PRIMARY KEY({2}, {3}, {4}, {5}, {6}, {7}, {8}),
-                FOREIGN KEY({4}, {5}, {6}) REFERENCES {1}({9}, {10}, {11})
-            )'''.format(
-                Database.DATABASE_TABLE_MONTHLY_PLANNINGS_WEEKS_DAYS,
-                Database.DATABASE_TABLE_MONTHLY_PLANNINGS,
-                'day_id_pk',  # TODO: how to deal with multispanning time_slot_types (ALL_WEEKEND)? nothing on day 6...
-                'week_id_pk',
-                'year_fk_pk',
-                'month_fk_pk',
-                'version_fk_pk',
-                'time_slot_type',
-                'activity_type',
-                COLUMN_YEAR_PK,
-                COLUMN_MONTH_PK,
-                COLUMN_VERSION_PK
-            ))
-
-            connection.execute('''CREATE TABLE IF NOT EXISTS {} (
-                year_fk INTEGER NOT NULL,
-                month_fk INTEGER NOT NULL,
-                version_fk INTEGER NOT NULL,
-                id_nephrologist_fk INTEGER NOT NULL,
-                timeslot_type INTEGER NOT NULL,
+                date_pk TEXT NOT NULL,
+                time_slot_type INTEGER NOT NULL,
                 activity_type INTEGER NOT NULL,
-                activity_count INTEGER NOT NULL,
-                PRIMARY KEY(year_fk ASC, month_fk ASC, version_fk ASC, id_nephrologist_fk ASC),
-                FOREIGN KEY(year_fk ASC, month_fk, version_fk) REFERENCES {}(year_pk, month_pk, version_pk),
+                id_nephrologist_fk INTEGER NOT NULL,
+                PRIMARY KEY(date_pk, month_pk),
                 FOREIGN KEY(id_nephrologist_fk) REFERENCES {}(id)
             )'''.format(
-                Database.DATABASE_TABLE_MONTHLY_PLANNINGS_NEPHROLOGISTS,
                 Database.DATABASE_TABLE_MONTHLY_PLANNINGS,
                 Database.DATABASE_TABLE_NEPHROLOGISTS
             ))

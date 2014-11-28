f2a4b4a97fc266cd956e957a3befe38034cb0a6e
christophe.duhamel.0909 <christophe.duhamel.0909@gmail.com>
2014-11-28 17:01:55 +0000

    implemented duration.py

--- Utils/constants.py
+++ Utils/constants.py
@@ -1,5 +1,32 @@
 __author__ = "Christophe"
 
+from datetime import timedelta
+from Enums.timeslot import TimeSlot
+
 
 class Constants:
     DATABASE_FILENAME_KEY = 'databaseFilename'
+
+    FIRST_SHIFT_LOWER_BOUND = 5
+    SECOND_SHIFT_LOWER_BOUND = 13
+    THIRD_SHIFT_LOWER_BOUND = 21
+    """ mapper between discrete time slots and analogous datetime
+
+        FIRST_SHIFT: from 05:00:00 to 12:59:59.999999
+        SECOND_SHIFT: from 13:00:00 to 20:59:59.999999
+        SECOND_SHIFT: from 21:00:00 to 04:59:59.999999
+    """
+    slots_temporally = {
+        TimeSlot.FIRST_SHIFT: (
+            timedelta(hours=FIRST_SHIFT_LOWER_BOUND),
+            timedelta(hours=SECOND_SHIFT_LOWER_BOUND, microseconds=-1)
+        ),
+        TimeSlot.SECOND_SHIFT: (
+            timedelta(hours=SECOND_SHIFT_LOWER_BOUND),
+            timedelta(hours=THIRD_SHIFT_LOWER_BOUND, microseconds=-1)
+        ),
+        TimeSlot.THIRD_SHIFT: (
+            timedelta(hours=THIRD_SHIFT_LOWER_BOUND),
+            timedelta(days=1, hours=FIRST_SHIFT_LOWER_BOUND, microseconds=-1)
+        )
+    }
